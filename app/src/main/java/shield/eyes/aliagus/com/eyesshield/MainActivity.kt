b48558371f300ae84880e83e0f72e4b7a64e3392
Ali Agus Hutapea <aliagushutapea@gmail.com>
2018-04-12 11:19:00 +0700

    first commit

---
+++ app/src/main/java/shield/eyes/aliagus/com/eyesshield/MainActivity.kt
@@ -0,0 +1,21 @@
+package shield.eyes.aliagus.com.eyesshield
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initUI()
+        setBrigness()
+    }
+
+    private fun initUI() {
+
+    }
+
+    private fun setBrigness() {
+    }
+}
