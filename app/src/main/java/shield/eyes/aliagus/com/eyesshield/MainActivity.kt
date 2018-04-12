55a2abbadedfc5947acdfe262b8a3356b8f4eda8
Ali Agus Hutapea <aliagushutapea@gmail.com>
2018-04-12 14:15:11 +0700

    buat fungsi seekbar

--- app/src/main/java/shield/eyes/aliagus/com/eyesshield/MainActivity.kt
+++ app/src/main/java/shield/eyes/aliagus/com/eyesshield/MainActivity.kt
@@ -1,21 +1,47 @@
 package shield.eyes.aliagus.com.eyesshield
 
 import android.os.Bundle
+import android.provider.Settings
 import android.support.v7.app.AppCompatActivity
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initUI()
         setBrightness()
     }
 
-    private fun initUI() {
-
-    }
-
     private fun setBrightness() {
+        seekBar_brightness.max = 225
+        var curBrightnessValue = 0
+        try {
+            curBrightnessValue = android.provider.Settings.System.getInt(contentResolver, android.provider.Settings.System.SCREEN_BRIGHTNESS)
+        } catch (e: Settings.SettingNotFoundException) {
+            e.printStackTrace()
+        }
+
+        var screenBrightness = curBrightnessValue
+        seekBar_brightness.progress = screenBrightness
+        seekBar_brightness.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            var progress = 0
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                progress = p1
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                android.provider.Settings.System.putInt(
+                        contentResolver,
+                        android.provider.Settings.System.SCREEN_BRIGHTNESS,
+                        progress
+                )
+            }
+        })
     }
 }
